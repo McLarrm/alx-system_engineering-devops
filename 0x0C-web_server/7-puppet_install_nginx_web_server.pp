0b86ce02bc3dedeef467a852de26022b63a9be2f
McLarrm <larrm_ola@live.com>
2023-09-26 17:02:20 -0700

    Configuration of Nginx server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,47 +1,20 @@
 # Install Nginx package
 package { 'nginx':
-  ensure => 'installed',
+  ensure => installed,
 }
 
-# Ensure Nginx is running and enabled
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx'],
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-# Create a custom HTML page with "Hello World!"
 file { '/var/www/html/index.html':
-  ensure  => 'file',
   content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
   require => Package['nginx'],
-}
-
-# Nginx configuration file for the root path
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-file { '/etc/nginx/sites-enabled/default':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-  notify => Service['nginx'],
-}
-
-file { '/etc/nginx/sites-available/redirect_me':
-  ensure  => 'file',
-  content => template('nginx/redirect_me.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-file { '/etc/nginx/sites-enabled/redirect_me':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/redirect_me',
-  require => File['/etc/nginx/sites-available/redirect_me'],
-  notify => Service['nginx'],
 }
