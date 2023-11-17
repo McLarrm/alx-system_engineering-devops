fc5d4a15f14620a950db8f0cc4286a9dbdde6f14
McLarrm <larrm_ola@live.com>
2023-11-16 17:10:48 -0800

    Sky is the limit, lets bring that limit higher

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Expand nginx user limit
+exec { 'expand nginx user limit and restart webserver':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => 'sed -i "s/15/4000/g" /etc/default/nginx; service nginx restart',
+}
